6b3a0feac4a88e4ddb121333205985d4c4e41e96
TABICHE Sohib <sohib.tabiche@hotmail.fr>
2024-02-17 20:40:13 +0100

    adding status

---
+++ redirect.php
@@ -0,0 +1,28 @@
+<?php
+
+    $url = $_GET['url'];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
+    curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch, CURLOPT_TIMEOUT,10);
+    $output = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    
+    if($httpcode =='200')
+    {
+        echo 'OK';
+        http_response_code(200);
+        exit;
+    }
+    else
+    {
+        echo 'KO';
+        http_response_code(500);
+        exit;
+    }
+    
+
+?>
